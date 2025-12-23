fd8635c261afece76190dc94c63603efffc7cf60
ChanaMandel <chanapeshamandel@gmail.com>
2025-12-23 18:21:32 -0500

    methods implemented

--- src/main/java/edu/touro/mcon264/sorting/InsertionSort.java
+++ src/main/java/edu/touro/mcon264/sorting/InsertionSort.java
@@ -1,11 +1,26 @@
 package edu.touro.mcon264.sorting;
 
 import java.util.Comparator;
+import java.util.Objects;
 
 public class InsertionSort implements Sorter {
 
     @Override
     public <T> void sort(T[] a, Comparator<? super T> comp) {
-        // TODO: implement insertion sort
+        Objects.requireNonNull(a, "array cannot be null");
+        Objects.requireNonNull(comp, "comparator cannot be null");
+
+            int n = a.length;
+            for (int i = 1; i < n;i++) {
+                T key = a[i];
+                int j = i - 1;
+
+                while (j >= 0 && comp.compare(a[j], key) > 0) {
+                    a[j + 1] = a[j];
+                    j --;
+                }
+                a[j + 1] = key;
+        }
+
     }
 }
